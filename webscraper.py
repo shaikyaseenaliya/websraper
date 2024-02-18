8495358fcf014abe0bbe84188ac4209b048ebd84
shaikyaseenaliya <157492467+shaikyaseenaliya@users.noreply.github.com>
2024-02-18 11:50:50 +0530

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,13 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
+req = requests.get("https://www.geeksforgeeks.org/")
+
+
+soup = BeautifulSoup(req.content,"html.parser")
+
+res = soup.title
+
+print(res.get_text())
